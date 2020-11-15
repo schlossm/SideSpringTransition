b9d93e338496d6ff9c475cd7e77a146fdbea6fee
Michael Schloss <schlossm@icloud.com>
2020-11-15 11:36:49 -0600

    Final lifecycle QOL updates
    * SideSpringDemo -> MSTransitionDemo
    * Dismissal now calls view controller lifecycle methods in the correct order, time, and count
    * Gesture uses `translation` instead of `location` to smoothen the initial start of the transition
    * Added comments

--- Sources/MSTransition/MSTransitionContainerView.swift
+++ Sources/MSTransition/MSTransitionContainerView.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
+/**
+ A custom container view controller that transitions views in a side-by-side animation.
+ 
+ The container contains built-in gesture support.  This gesture functions the same as `UINavigationController`
+ */
 public class MSTransitionContainerViewController : UIViewController
 {
     // Order preserved
-    private var trackedChildren = [UIViewController]()
+    private var _trackedChildren = [UIViewController]()
+    private var trackedChildren: [UIViewController]
+    {
+        get { _trackedChildren }
+        _modify
+        {
+            yield &_trackedChildren
+            screenEdgeGesture.isEnabled = _trackedChildren.count > 1
+        }
+        set { _trackedChildren = newValue }
+    }
     
     private var screenEdgeGesture = UIScreenEdgePanGestureRecognizer()
     private var activeAnimator : UIViewPropertyAnimator?
+    
+    public override var shouldAutomaticallyForwardAppearanceMethods: Bool { false }
     
     override public func viewDidLoad() {
         super.viewDidLoad()
@@ -23,14 +40,27 @@ public class MSTransitionContainerViewController : UIViewController
         view.addGestureRecognizer(screenEdgeGesture)
     }
     
+    /**
+     Presents a view controller onto this container.
+     
+     Presentation follows the following rules:
+     1. If the container is currently empty, the provided view controller is placed on screen in the final position with no animation
+     2. If the container currently has at least one view controller in its stack, a side-by-side transition occurs
+         * If `animated` is false, the animations are skipped
+     
+     - Parameter viewControllerToPresent: The view controller to place in the container
+     - Parameter animated: Whether or not the transition shows an animation.  If this is the first presentation in the container, the value of this parameter is ignored
+     */
     public func present(_ viewControllerToPresent: UIViewController, animated: Bool = true)
     {
+        viewControllerToPresent.beginAppearanceTransition(true, animated: trackedChildren.isEmpty ? false : animated)
         addToContainer(viewControllerToPresent)
         
         guard let from = trackedChildren.last else
         {
             trackedChildren = [viewControllerToPresent]
             viewControllerToPresent.didMove(toParent: self)
+            viewControllerToPresent.endAppearanceTransition()
             return
         }
         trackedChildren.append(viewControllerToPresent)
@@ -39,11 +69,13 @@ public class MSTransitionContainerViewController : UIViewController
             self.view.isUserInteractionEnabled = true
             from.view.removeFromSuperview()
             from.removeFromParent()
-            from.didMove(toParent: nil)
+            from.endAppearanceTransition()
+            viewControllerToPresent.endAppearanceTransition()
             viewControllerToPresent.didMove(toParent: self)
             self.activeAnimator = nil
         }
         
+        from.beginAppearanceTransition(false, animated: animated)
         view.isUserInteractionEnabled = false
         from.willMove(toParent: nil)
         if !animated
@@ -62,8 +94,23 @@ public class MSTransitionContainerViewController : UIViewController
         animator.startAnimation()
     }
     
-    public func dismiss(viewController: UIViewController? = nil)
+    /**
+     Dismisses a provided view controller
+     
+     Dismissal follows the following rules:
+     1. The top view controller cannot be dismissed, and if provided an `NSException` will be raised
+     2. The optionally provided view controller must be within the container's stack, if not an `NSException` will be raised
+     3. If no view controller is provided, the container will dismiss the currently-visible (top) view controller
+         * If `animated` is false, the animations are skipped
+     4. If a view controller is provided, the container animates the transition between the top view controller and the view controller in the stack underneath the provided view controller
+         * If `animated` is false, the animations are skipped
+     
+     - Parameter viewController: An optionally provided view controller to dismiss
+     - Parameter animated: Whether or not the transition shows an animation
+     */
+    public func dismiss(viewController: UIViewController? = nil, animated: Bool = true)
     {
+        guard trackedChildren.count > 1 else { return }
         guard viewController != trackedChildren.first else
         {
             if let vc = viewController
@@ -76,13 +123,21 @@ public class MSTransitionContainerViewController : UIViewController
             }
             return
         }
+        if let vc = viewController
+        {
+            guard trackedChildren.contains(vc) else
+            {
+                withVaList([vc]) { NSException.raise(.invalidArgumentException, format: "Cannot dismiss %@, which is not in the container hierarchy", arguments: $0) }
+                return
+            }
+        }
         let current = trackedChildren.last!
         let animator = UIViewPropertyAnimator(duration: 0.5, dampingRatio: 1.0) { [self] in current.view.transform = .init(translationX: view.bounds.width, y: 0.0) }
         
         let index: Int = {
             if let vc = viewController
             {
-                return trackedChildren.index(before: trackedChildren.firstIndex { $0 == vc }!)
+                return trackedChildren.index(before: trackedChildren.firstIndex(of: vc)!)
             }
             else
             {
@@ -91,7 +146,16 @@ public class MSTransitionContainerViewController : UIViewController
         }()
         let to = trackedChildren[index]
         addToContainer(to)
-        to.viewWillAppear(true)
+        to.beginAppearanceTransition(true, animated: animated)
+        current.beginAppearanceTransition(false, animated: animated)
+        if !animated
+        {
+            current.endAppearanceTransition()
+            to.endAppearanceTransition()
+            trackedChildren = Array(trackedChildren[0...index])
+            return
+        }
+        
         to.view.transform = CGAffineTransform(translationX: -view.bounds.width, y: 0.0)
         animator.addAnimations { to.view.transform = .identity }
         animator.addCompletion
@@ -99,23 +163,15 @@ public class MSTransitionContainerViewController : UIViewController
             switch $0
             {
             case .end:
-                current.viewDidDisappear(true)
-                to.viewDidAppear(true)
-                for vc in trackedChildren[(index + 1)..<trackedChildren.endIndex]
-                {
-                    vc.willMove(toParent: nil)
-                    vc.view.removeFromSuperview()
-                    vc.removeFromParent()
-                    vc.didMove(toParent: nil)
-                }
+                current.endAppearanceTransition()
+                to.endAppearanceTransition()
                 trackedChildren = Array(trackedChildren[0...index])
                 
             case .start:
-                current.viewDidAppear(true)
-                to.willMove(toParent: nil)
-                to.view.removeFromSuperview()
-                to.removeFromParent()
-                to.didMove(toParent: nil)
+                current.beginAppearanceTransition(true, animated: animated)
+                to.beginAppearanceTransition(false, animated: animated)
+                current.endAppearanceTransition()
+                to.endAppearanceTransition()
                 
             default: break
             }
@@ -131,14 +187,16 @@ public class MSTransitionContainerViewController : UIViewController
         case .began:
             dismiss()
             activeAnimator?.pauseAnimation()
+            let locationX = gesture.translation(in: view).x
+            activeAnimator?.fractionComplete = locationX / view.bounds.width
             
         case .changed:
-            let locationX = gesture.location(in: view).x
+            let locationX = gesture.translation(in: view).x
             activeAnimator?.fractionComplete = locationX / view.bounds.width
             
         case .ended:
             let velocityX = gesture.velocity(in: view).x
-            let locationX = gesture.location(in: view).x
+            let locationX = gesture.translation(in: view).x
             switch locationX
             {
             case 0 ..< view.bounds.width / 2.0:
